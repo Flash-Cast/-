aecb352cacee593797389579a8224d52b07097f8
Flash-Cast <rivermouthlake@gmail.com>
2024-12-06 09:56:02 +0900

    Add files via upload

--- 掲示板.py
+++ 掲示板.py
@@ -9,3 +9,10 @@ def bbs():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    pub_date = db.Column(db.DateTime, nullable=False,
+                                default=datetime.utcnow)
+    name = db.Column(db.Text())
+    article = db.Column(db.Text())
